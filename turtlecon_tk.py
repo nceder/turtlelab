0879b5bbf43d25307d01c024e24b9ebea280acbb
doc <doc@paladin>
2009-12-26 16:43:36 -0500

    added colors file, various fix-ups

--- turtlecon_tk.py
+++ turtlecon_tk.py
@@ -11,7 +11,7 @@ from Tkinter import *
 #from pmw import *
 import sys
 color_list = ["red", "green", "blue", "brown"]
-def go():
+def go(event=None):
     """ compile code to code object and run """
     code_text = codebox.get(0.0,END)
     print code_text
@@ -32,15 +32,23 @@ def code_clear():
 def set_color():
 #    print dir(colors)
 #    print colors.selection_get(ACTIVE)
-    color_str  = """\ncolor('%s')\n""" % (color_list[colors.index(ACTIVE)])
+    color_str  = """color('%s')""" % (color_list[colors.index(ACTIVE)])
     codebox.insert(END, color_str)
+    go()
+#    print colors.keys()
                                      
 def popup():
     colors.post(color_btn.winfo_rootx(), color_btn.winfo_rooty())
 
+def click(event=None, event2=None):
+    print event, event2
+    pencolor('black')
+    width(width()+2)
+
+
 #TODO: make control window stay on top unless minimized
 #TODO: make turtle window stay on top unless minimized
-#TODO: colorize code - keywords, strings, comments
+#TODO: colorize code - keywords, strings, comment
 #TODO: add turtle color, size and image manipulation
 #TODO: reset screen or even close screen
 #TODO: help?
@@ -60,6 +68,7 @@ history_controls.pack(fill=X)
 code_label = Label(root, text="Code")
 code_label.pack()
 codebox = Text(root, height=15, width=80)
+## codebox.bind("<Return>", go)
 codebox.pack()
 code_controls = Frame(root, borderwidth=2, relief='sunken')
 go_btn = Button(code_controls, text="Go!", command=go)
@@ -73,4 +82,5 @@ colors = Menu(code_controls, tearoff=0)
 for color_name in color_list:
     colors.add_command(label=color_name, command=set_color)
 #colors.pack(side=LEFT)
+onclick(click)
 root.mainloop()
